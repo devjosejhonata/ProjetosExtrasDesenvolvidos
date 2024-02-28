fba95030dc43364cbc868ad969e8ae4830248c18
Jose Jhonata <contatojosejhonata@gmail.com>
2024-02-27 22:51:22 -0300

    projetos extras desenvolvidos, commit 1

---
+++ Calculadora_de_Gorjeta/scripts.js
@@ -0,0 +1,21 @@
+//<!-- Projeto de estudo - José Jhonata - Praticando JavaScript -->
+
+function calculateTip() {
+  // Obter valor da conta e qualidade do serviço
+  const billAmount = document.getElementById("billAmount").value;
+  const serviceQuality = document.getElementById("serviceQuality").value;
+
+  // Validar entrada
+  if (billAmount === "" || serviceQuality == 0) {
+    alert("Por favor, preencha os campos!");
+    return;
+  }
+
+  // Calcular gorjeta e valor total
+  const tipAmount = billAmount * serviceQuality;
+  const totalAmount = parseFloat(billAmount) + parseFloat(tipAmount);
+
+  // Exibir resultados
+  document.getElementById("tipAmount").value = "R$" + tipAmount.toFixed(2);
+  document.getElementById("totalAmount").value = "R$" + totalAmount.toFixed(2);
+}
